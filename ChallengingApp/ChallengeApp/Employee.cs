e4e02e1c0aa5b769039a49657e7f7eb328af6568
kamil osyp <kamilosyp@gmail.com>
2023-07-15 17:08:41 +0200

    nowe 7

---
+++ ChallengingApp/ChallengeApp/Employee.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<int> score = new List<int>();
+
+        public Employee(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+
+        public string Name { get; private set; }
+        public string surname { get; private set; }
+        public int age { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+
+        }
+        public void Addscore(int number)
+        {
+            this.score.Add(number);
+        }
+    }
+}
---
+++ ChallengingApp/ChallengeApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using ChallengeApp;
+
+Employee employee1 = new Employee("Kamil", "Bart", 33);
+Employee employee2 = new Employee("Monika", "Kot", 22);
+Employee employee3 = new Employee("Kuba", "Jeż", 45);
+
+employee1.Addscore(2);
+employee1.Addscore(5);
+employee1.Addscore(3);
+employee1.Addscore(6);
+
+employee2.Addscore(1);
+employee2.Addscore(2);
+employee2.Addscore(3);
+employee2.Addscore(4);
+
+employee3.Addscore(5);
+employee3.Addscore(5);
+employee3.Addscore(6);
+employee3.Addscore(2);
+
+List<Employee> employees = new List<Employee>()
+{
+    employee1, employee2, employee3
+};
+var maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach (var employee in employees)
+{
+    if (employee.Result > maxResult)
+    {
+        employeeWithMaxResult = employee;
+        maxResult = employee.Result;
+    }
+}
+
+Console.WriteLine(employeeWithMaxResult.Name + " " + employeeWithMaxResult.surname + " " + "ma" + " " + employeeWithMaxResult.age + "lata" + " i" + employeeWithMaxResult.Result + "" + "punktów");
