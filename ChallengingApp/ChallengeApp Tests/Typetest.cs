f97c385c888d786a92c61fbf0551bd50511806b7
kamil osyp <kamilosyp@gmail.com>
2023-07-20 10:10:11 +0200

    test7

---
+++ ChallengingApp/ChallengeApp Tests/Typetest.cs
@@ -0,0 +1,40 @@
+﻿
+
+using ChallengeApp;
+
+namespace ChallengeApp_Tests
+{
+    public class Typetest
+    {
+
+        [Test]
+
+        public void Employeescorecheckwhenpositive_shouldreturnCorrectsum()
+        {
+
+            var user1 = new Employee("Kamil", "Bolek", 33);
+
+            user1.Addscore(4);
+            user1.Addscore(3);
+            user1.Addscore(6);
+
+            var result1 = user1.Result;
+
+            Assert.AreEqual(13, result1);
+        }
+
+        [Test]
+        public void Employeescorecheckreduce_shouldreurtnCorrectsum()
+        {
+
+            var user2 = new Employee("monika", "gałka", 22);
+            user2.Addscore(4);
+            user2.Addscore(-6);
+            user2.Addscore(-1);
+
+            var result2 = user2.Result;
+
+            Assert.AreEqual(-3, result2);
+        }
+    }
+}
