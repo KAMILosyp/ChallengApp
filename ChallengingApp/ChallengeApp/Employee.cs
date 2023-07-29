7f2a716341e938cc4cfa7a3904d36252dfb88276
kamil osyp <kamilosyp@gmail.com>
2023-07-29 08:59:54 +0200

    zadanie 9

--- ChallengingApp/ChallengeApp/Employee.cs
+++ ChallengingApp/ChallengeApp/Employee.cs
@@ -1,34 +1,43 @@
-﻿
-
-
-namespace ChallengeApp
+﻿namespace ChallengeApp
 {
     public class Employee
     {
-        private List<int> score = new List<int>();
-
-        public Employee(string name, string surname, int age)
+        private List<float> grades = new List<float>();
+        
+        public Employee(string name, string surname)
         {
             this.Name = name;
-            this.surname = surname;
-            this.age = age;
+            this.Surname = surname;
         }
 
         public string Name { get; private set; }
-        public string surname { get; private set; }
-        public int age { get; private set; }
 
-        public int Result
+        public string Surname { get; private set; }
+
+        public void AddGrade(float grade)
         {
-            get
+            this.grades.Add(grade);
+        }
+
+        public Statistics GetStatistics()
+        {
+            var statistics = new Statistics();
+
+            statistics.Average = 0;
+            statistics.Max = float.MinValue;
+            statistics.Min = float.MaxValue;
+
+            foreach(var grade in this.grades)
             {
-                return this.score.Sum();
+                statistics.Max = Math.Max(statistics.Max, grade);
+                statistics.Min = Math.Min(statistics.Min, grade);
+                statistics.Average += grade;
             }
 
-        }
-        public void Addscore(int number)
-        {
-            this.score.Add(number);
+            statistics.Average = statistics.Average / this.grades.Count;
+
+
+            return statistics;
         }
     }
 }
